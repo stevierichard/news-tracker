082ad118ffbfec738b91f0509f9610281087b61b
Steven Simon <stevierichard1@gmail.com>
2020-09-13 22:53:31 -0700

    created NavBar

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div class="jumbotron jumbotron-fluid bg-info">
+      <div class="container">
+        <h1 class="display-5 text">News Tracker App</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
